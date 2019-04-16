7b7447f1b5f148ddd74bd6d7883863aa810332e5
haimenggan.github.io <haimeng95@gmail.com>
2019-04-15 21:55:33 -0700

    changed hero

--- js/script.js
+++ js/script.js
@@ -1,28 +1,17 @@
 $(document).ready(function () {
-    $("#mycarousel").carousel({ interval: 2000 });
-    $("#carousel-button").click(function () {
-        if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-            $("#mycarousel").carousel('pause');
-            $("#carousel-button").children("span").removeClass('fa-pause');
-            $("#carousel-button").children("span").addClass('fa-play');
-        }
-        else if ($("#carousel-button").children("span").hasClass('fa-play')) {
-            $("#mycarousel").carousel('cycle');
-            $("#carousel-button").children("span").removeClass('fa-play');
-            $("#carousel-button").children("span").addClass('fa-pause');
-        }
-    });
+    $("#mycarousel").carousel({ interval: 4000 });
 });
 
 $(document).ready(function () {
-    $('#panel-faq').click(function () {
-        if ($('#panel-faq').children('span').hasClass('fa-plus')) {
-            $('#panel-faq').children('span').removeClass('fa-plus');
-            $('#panel-faq').children('span').addClass('fa-minus');
+    $('#collapse-button').click(function () {
+        if ($('#collapse-button').children('span').hasClass('fa-plus')) {
+            $()
+            $('#collapse-button').children('span').removeClass('fa-plus');
+            $('#collapse-button').children('span').addClass('fa-minus');
         }
-        else if ($('#panel-faq').children('span').hasClass('fa-mius')) {
-            $('#panel-faq').children('span').removeClass('fa-minus');
-            $('#panel-faq').children('span').addClass('fa-plus');
+        else if ($('#collapse-button').children('span').hasClass('fa-mius')) {
+            $('#collapse-button').children('span').removeClass('fa-minus');
+            $('#collapse-button').children('span').addClass('fa-plus');
         }
     });
 });
